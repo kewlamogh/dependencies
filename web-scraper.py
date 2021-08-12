97ceebcd67a3df41916c06309e3d4f344dbee541
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-08-12 16:31:07 -0700

    Update web-scraper.py

--- web-scraper.py
+++ web-scraper.py
@@ -1,1 +1,10 @@
-
+import bs4, requests
+class WebScraper():
+  def __init__(self, website, config):
+    reqs = requests.get(website)
+    if config[0]:
+      self.soup = bs4.BeautifulSoup(reqs.text, 'html.parser', parse_only=bs4.SoupStrainer(config[1]))
+    else:
+      self.soup = bs4.BeautifulSoup(reqs.text, 'html.parser') # strains for all span elements
+  def getElementsByClass(className):
+    return self.soup.find_all(class_ = className)
